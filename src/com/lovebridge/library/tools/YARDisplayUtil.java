5765c6c2ac6d2cee1a9691a39f81b91cbe1ccb3e
yushilong <yushilong0921@126.com>
2014-12-16 21:36:16 +0800

    update

--- src/com/lovebridge/library/tools/YARDisplayUtil.java
+++ src/com/lovebridge/library/tools/YARDisplayUtil.java
@@ -1,5 +1,6 @@
 package com.lovebridge.library.tools;
 
+import android.content.Context;
 import android.util.DisplayMetrics;
 import com.lovebridge.application.MainApplication;
 
@@ -27,5 +28,17 @@ public class YARDisplayUtil
     public static int getScreenHeight()
     {
         return displayMetrics.heightPixels;
+    }
+
+    public static int dip2px(Context context, float dpValue)
+    {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, float pxValue)
+    {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
     }
 }
--- src/com/lovebridge/widget/CustomItem.java
+++ src/com/lovebridge/widget/CustomItem.java
@@ -10,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.lovebridge.R;
+import com.lovebridge.library.tools.YARDisplayUtil;
 
 /**
  *author: lscm
@@ -37,29 +38,45 @@ public class CustomItem extends LinearLayout
 
     public void initView(final Context context, AttributeSet attrs)
     {
-        ViewGroup viewGroup = (ViewGroup) LayoutInflater.from(context).inflate(R.layout.custom_mineitem, this,true);
+        ViewGroup viewGroup = (ViewGroup) LayoutInflater.from(context).inflate(R.layout.custom_mineitem, this, true);
         ImageView iv_leftIcon = (ImageView) viewGroup.findViewById(R.id.iv_lefticon);
         TextView tv_middleUp = (TextView) viewGroup.findViewById(R.id.tv_middleUp);
         TextView tv_middleDown = (TextView) viewGroup.findViewById(R.id.tv_middleDown);
         TextView tv_right = (TextView) viewGroup.findViewById(R.id.tv_right);
         ImageView iv_rightIcon = (ImageView) viewGroup.findViewById(R.id.iv_righticon);
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.mineitem);
-        iv_leftIcon.setImageResource(typedArray.getResourceId(R.styleable.mineitem_left_icon, 0));
-        tv_middleUp.setText(typedArray.getText(R.styleable.mineitem_middle_upText));
-        tv_middleDown.setText(typedArray.getText(R.styleable.mineitem_middle_downText));
-        tv_middleUp.setTextColor(typedArray.getColor(R.styleable.mineitem_middle_upTextColor, android.R.color.black));
-        tv_middleDown.setTextColor(typedArray.getColor(R.styleable.mineitem_middle_upTextColor, android.R.color.black));
-        tv_right.setText(typedArray.getText(R.styleable.mineitem_right_text));
-        iv_rightIcon.setImageResource(typedArray.getResourceId(R.styleable.mineitem_right_icon, 0));
-        RelativeLayout.LayoutParams leftIconLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        int imgSize = (int) typedArray.getDimension(R.styleable.mineitem_left_iconSize, 30);
-        leftIconLayoutParams.width = imgSize;
-        leftIconLayoutParams.height = imgSize;
-        iv_leftIcon.setLayoutParams(leftIconLayoutParams);
-        RelativeLayout.LayoutParams rightIconLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        rightIconLayoutParams.width = imgSize;
-        rightIconLayoutParams.height = imgSize;
-        iv_rightIcon.setLayoutParams(rightIconLayoutParams);
+        if (typedArray.hasValue(R.styleable.mineitem_left_icon))
+            iv_leftIcon.setImageResource(typedArray.getResourceId(R.styleable.mineitem_left_icon, 0));
+        if (typedArray.hasValue(R.styleable.mineitem_middle_upText))
+            tv_middleUp.setText(typedArray.getText(R.styleable.mineitem_middle_upText));
+        if (typedArray.hasValue(R.styleable.mineitem_middle_downText))
+            tv_middleDown.setText(typedArray.getText(R.styleable.mineitem_middle_downText));
+        if (typedArray.hasValue(R.styleable.mineitem_middle_upTextColor))
+            tv_middleUp.setTextColor(typedArray.getColor(R.styleable.mineitem_middle_upTextColor, android.R.color.black));
+        if (typedArray.hasValue(R.styleable.mineitem_middle_downTextColor))
+            tv_middleDown.setTextColor(typedArray.getColor(R.styleable.mineitem_middle_downTextColor, android.R.color.black));
+        if (typedArray.hasValue(R.styleable.mineitem_middle_down_visible))
+            tv_middleDown.setVisibility(typedArray.getBoolean(R.styleable.mineitem_middle_down_visible, false) ? VISIBLE : GONE);
+        if (typedArray.hasValue(R.styleable.mineitem_right_text))
+            tv_right.setText(typedArray.getText(R.styleable.mineitem_right_text));
+        if (typedArray.hasValue(R.styleable.mineitem_right_icon))
+            iv_rightIcon.setImageResource(typedArray.getResourceId(R.styleable.mineitem_right_icon, 0));
+        if (typedArray.hasValue(R.styleable.mineitem_left_iconSize))
+        {
+            RelativeLayout.LayoutParams leftIconLayoutParams = (RelativeLayout.LayoutParams) iv_leftIcon.getLayoutParams();
+            int imgSize = (int) typedArray.getDimension(R.styleable.mineitem_left_iconSize, YARDisplayUtil.dip2px(context, 20));
+            leftIconLayoutParams.width = imgSize;
+            leftIconLayoutParams.height = imgSize;
+            iv_leftIcon.setLayoutParams(leftIconLayoutParams);
+        }
+        if (typedArray.hasValue(R.styleable.mineitem_right_iconSize))
+        {
+            RelativeLayout.LayoutParams rightIconLayoutParams = (RelativeLayout.LayoutParams) iv_rightIcon.getLayoutParams();
+            int imgSize = (int) typedArray.getDimension(R.styleable.mineitem_right_iconSize, YARDisplayUtil.dip2px(context, 20));
+            rightIconLayoutParams.width = imgSize;
+            rightIconLayoutParams.height = imgSize;
+            iv_rightIcon.setLayoutParams(rightIconLayoutParams);
+        }
         typedArray.recycle();
     }
 }
